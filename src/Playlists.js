ff1298115857cb9b55aae2272a730e37ab7cd415
Paul Picazo <ppicazo@gmail.com>
2017-02-14 17:49:17 -0800

    * Playlists, not just the first one (get a warning about 'key' prop though for the array) * Change the logo (probably will have to change it again) * Ability to login

---
+++ src/Playlists.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Api from './Api';
+
+class Playlists extends Component {
+  constructor() {
+    super()
+    this.state = {
+      playlists: []
+    }
+  }
+
+  loadData() {
+    var api = new Api();
+    api.fetchPlaylists().then(json => {
+      this.setState({
+        playlists: json
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  render() {
+    var playlists = this.state.playlists
+    return (
+      <div className="Playlists">
+        {playlists.map(playlist =>
+            <div className="Playlist">
+              <a href={"https://open.spotify.com/user/" + playlist.owner_id + "/playlist/" + playlist.id}>{playlist.name}</a>
+            </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Playlists;
--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Api from './Api';
-import Playlist from './Playlist'
+import Playlists from './Playlists'
 
 class Profile extends Component {
   constructor() {
@@ -26,14 +26,26 @@ class Profile extends Component {
   }
 
   render() {
-    return (
-      <div className="Profile">
-        <p className="App-intro">
-          Spotify User Id: {this.state.profile.id}
-        </p>
-        <Playlist />
-      </div>
-    );
+    if (this.state.profile.id != null) {
+      return (
+          <div className="Profile">
+            <p className="App-intro">
+              Hello {this.state.profile.id}
+            </p>
+            <Playlists />
+          </div>
+      );
+    } else {
+      return (
+        <div>
+          <p>Archive your <i>Discover Weekly</i> every week in to a single playlist, and the only Spotify permission this app needs is to read-only on playlists.</p>
+          <form method="POST" action="https://discoverindefinitely.com/login">
+            <input type="hidden" name="app_base_url" value={window.location} />
+            <input type="submit" value="Login with Spotify" />
+          </form>
+        </div>
+      );
+    }
   }
 }
 
